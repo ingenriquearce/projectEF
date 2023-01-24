2fb029c50eaea97dd0fcb5477d2ad0420e5d8213
Enrique Arce <enrique7arce.eaa@gmail.com>
2023-01-24 15:13:05 -0400

    realizamos acciones de get put y delete a la base de datos

--- Models/categoria.cs
+++ Models/categoria.cs
@@ -1,17 +1,20 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace projectEF.Models;
 
 public class Categoria
 {
-    [Key]
+    //[Key]
     public Guid CategoriaId{get;set;}
 
-    [Required]
-    [MaxLength(150)]
+    //[Required]
+    //[MaxLength(150)]
     public string Nombre{get;set;}
 
     public string Descripcion{get;set;}
 
+    //To avoid postman error
+    [JsonIgnore]
     public virtual ICollection<Tarea> Tareas{get;set;}
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using projectEF;
+using projectEF.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,48 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria " + dbContext.Database.IsInMemory());
+});
+
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext)=>
+{
+    return Results.Ok(dbContext/*.Tareas //We comment this value because we need to be connected with a database*/);
+    //return Results.Ok(dbContext.Tareas.Include(p=> p.Categoria).Where(p=> p.PrioridadTarea == projectEF.Models.Prioridad.baja));
+});
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea,[FromRoute] Guid id)=>
+{
+    //We need to connect us to the datbase to use "Tareas" collection
+    
+    var tareaActual = dbContext/*.Tareas.Find(id)*/;
+
+    if(tareaActual!=null)
+    {
+        /*tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;*/
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();   
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+    //We need to connect us to the datbase to use "Tareas" collection
+    var tareaActual = dbContext/*.Tareas.Find(id)*/;
+
+    if(tareaActual!=null)
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
